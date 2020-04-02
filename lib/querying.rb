5f3ee7362529c5be0058350732f5d1c8abf7946c
Willis Aybar <willisaybar@gmail.com>
2020-04-02 00:55:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,8 +25,9 @@ end
 #select character motto
 #max motto
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, MAX(motto)
-    FROM characters"
+  "SELECT name, motto
+    FROM characters
+    ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
