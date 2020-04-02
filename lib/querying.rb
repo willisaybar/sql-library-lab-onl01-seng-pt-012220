7822a5818a3980331b665a291996da59926bb086
Willis Aybar <willisaybar@gmail.com>
2020-04-02 01:00:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,9 +32,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, 
-    FROM characters
-    ORDER BY LENGTH(species)"
+  "SELECT species, COUNT (*) FROM characters GROUP BY species ORDER BY COUNT (species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
