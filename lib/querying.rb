0ea8ff75a8e0eca0576d7e41440edd0eb14ef82c
Willis Aybar <willisaybar@gmail.com>
2020-04-02 01:00:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, 
+    FROM characters
+    ORDER BY LENGTH(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
